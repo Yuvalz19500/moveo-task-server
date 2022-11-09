9243b36802d9a3109ab10df4faff924b6e1c4b81
Yuval <yuvalz19500@gmail.com>
2022-11-10 01:17:07 +0200

    prod

--- src/assets/migrations/1-1667916818451-UserMigration.ts
+++ src/assets/migrations/1-1667916818451-UserMigration.ts
@@ -2,9 +2,6 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class UserMigration1667916818451 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(
-      `ALTER TABLE moveo.user RENAME COLUMN passowrd TO password;`,
-    );
     await queryRunner.query(`INSERT INTO moveo.user (id, username, password, userType) VALUES
         (1, 'yuval_student', '$2a$10$kkZBYXxojVDKM5z/aiqhtO63cGjwXznRWsyGEAjaG8y0pPi9Jf6Zq', 'STUDENT'),
         (2, 'yuval_mentor', '$2a$10$kkZBYXxojVDKM5z/aiqhtO63cGjwXznRWsyGEAjaG8y0pPi9Jf6Zq', 'MENTOR');`);
